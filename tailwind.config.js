852b987f53c0a51b2d2e2280d622d93c1a549eab
naveen-sharma15 <naveensharma8541@gmail.com>
2023-10-30 23:44:46 +0530

    cnd added

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    
+    extend: {},
+    fontFamily: {
+      'Poppins': ['Poppins', 'sans-serif'],
+      
+    },
+  },
+  plugins: [],
+}
+
